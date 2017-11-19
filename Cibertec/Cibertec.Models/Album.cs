3aa5cbc2028a054a7d995db0a6f864189069eaad
Juan Cucho <juan.cucho@outlook.com>
2017-11-19 09:07:56 -0500

    Se creo capa models de las tablas Album, Artist, Customer y Employee

---
+++ Cibertec/Cibertec.Models/Album.cs
@@ -0,0 +1,12 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Cibertec.Models
+{
+    public class Album
+    {
+        [ExplicitKey]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+    }
+}
---
+++ Cibertec/Cibertec.Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Cibertec.Models
+{
+    public class Artist
+    {
+        [ExplicitKey]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
