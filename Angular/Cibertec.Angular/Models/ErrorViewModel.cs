0dae64ebb887dbcecd7bf44b163d4d206dee9f0c
Juan Cucho <juan.cucho@outlook.com>
2017-11-20 02:06:20 -0500

    Se creo una nueva solucion para el proyecto AngularJS

---
+++ Angular/Cibertec.Angular/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cibertec.Angular.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
